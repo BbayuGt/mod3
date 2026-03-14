6589d82db78c0ab28363a71b83aaac2cfb270935
BbayuGt <BbayuGt@DESKTOP-UD2HIE2>
2026-03-14 01:05:21 +0000

    Menambahkan tampilan GUI

--- Modul3_103022400085/Form1.Designer.cs
+++ Modul3_103022400085/Form1.Designer.cs
@@ -28,12 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            satuanAwal = new ComboBox();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // satuanAwal
+            // 
+            satuanAwal.DisplayMember = "a";
+            satuanAwal.FormattingEnabled = true;
+            satuanAwal.Items.AddRange(new object[] { "Celcius", "Fahrenheit", "Kelvin", "Reamur" });
+            satuanAwal.Location = new Point(114, 201);
+            satuanAwal.Name = "satuanAwal";
+            satuanAwal.Size = new Size(121, 23);
+            satuanAwal.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(114, 183);
+            label1.Name = "label1";
+            label1.Size = new Size(72, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Satuan Awal";
+            label1.Click += label1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(296, 201);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(296, 183);
+            label2.Name = "label2";
+            label2.Size = new Size(72, 15);
+            label2.TabIndex = 3;
+            label2.Text = "Satuan Awal";
+            label2.Click += label2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(satuanAwal);
+            Name = "Form1";
             Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private ComboBox satuanAwal;
+        private Label label1;
+        private TextBox textBox1;
+        private Label label2;
     }
 }
